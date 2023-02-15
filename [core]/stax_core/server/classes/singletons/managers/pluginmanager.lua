41869eae832097b2e935579d461400220f3d96cb
Xander Harrison <xander.harrison77@gmail.com>
2023-02-15 06:01:33 -0500

    Shortened method names

--- [core]/stax_core/server/classes/singletons/managers/pluginmanager.lua
+++ [core]/stax_core/server/classes/singletons/managers/pluginmanager.lua
@@ -4,15 +4,15 @@ local PluginManager = {}
 --- Gets the plugin using its key
 ---@param key string
 ---@return Plugin
-function PluginManager.GetPlugin(key)
-  return exports.stax_core:PluginManager_GetPlugin(key)
+function PluginManager.Get(key)
+  return exports.stax_core:PluginManager_Get(key)
 end
 
 --- Gets the plugins key
 ---@param resource string
 ---@return string
-function PluginManager.GetPluginKey(resource)
-  return exports.stax_core:PluginManager_GetPluginKey(resource)
+function PluginManager.GetKey(resource)
+  return exports.stax_core:PluginManager_GetKey(resource)
 end
 
 Stax.ServerOnly(function()
