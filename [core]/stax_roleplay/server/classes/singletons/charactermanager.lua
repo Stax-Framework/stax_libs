b2c61d6d692e53b9c3d3f3f39358d5917c997113
Xander Harrison <xander.harrison77@gmail.com>
2023-02-18 12:05:16 -0500

    Added params and return

--- [core]/stax_roleplay/server/classes/singletons/charactermanager.lua
+++ [core]/stax_roleplay/server/classes/singletons/charactermanager.lua
@@ -1,14 +1,21 @@
 ---@class CharacterManager
 local CharacterManager = {}
 
+--- Adds a character to the character manager
+---@param character RPCharacter
 function CharacterManager.Add(character)
   exports.stax_roleplay:CharacterManager_Add(character)
 end
 
+--- Removes a character from the character manager
+---@param character RPCharacter
 function CharacterManager.Remove(character)
   exports.stax_roleplay:CharacterManager_Remove(character)
 end
 
+--- Gets a character from the character manager
+---@param handle number
+---@return RPCharacter
 function CharacterManager.Get(handle)
   return exports.stax_roleplay:CharacterManager_Get(handle)
 end
